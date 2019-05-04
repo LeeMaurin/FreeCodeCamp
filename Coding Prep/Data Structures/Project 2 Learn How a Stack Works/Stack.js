335e3ef15e83b040e91084cea84123824e44900a
Lee Maurin <leem4421@gmail.com>
2019-05-04 16:04:44 -0500

    Add files via upload

---
+++ Coding Prep/Data Structures/Project 2 Learn How a Stack Works/Stack.js
@@ -0,0 +1,14 @@
+// Assignment:
+// Modify the given array and treat it like a stack
+// using the JavaScript methods mentioned above.
+// Remove the top element "PSY44" from the stack.
+// Then add "CS50" to be the new top element of the stack.
+
+var homeworkStack = ['BIO12', 'HIS80', 'MAT122', 'PSY44'];
+// Only change code below this line
+
+// Removes the top element "PSY44"
+homeworkStack.pop();
+
+// Adds a new top element "CS50"
+homeworkStack.push('CS50');
