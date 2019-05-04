553df7cf266c3bb5fab23686e9c704baafe196f7
Lee Maurin <leem4421@gmail.com>
2019-05-04 13:40:35 -0500

    Update fibEvenSum.js

--- Coding Prep/Project Euler/Project 2 Even Fibonacci Numbers/fibEvenSum.js
+++ Coding Prep/Project Euler/Project 2 Even Fibonacci Numbers/fibEvenSum.js
@@ -2,13 +2,16 @@
 // The output is a summation of the even numbers in the
 // fibonacci series.
 
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node fibEvenSum.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
 function fiboEvenSum(number) {
   var list = [1, 1];
   for (var i = 1; i <= number; i++) {
     let a = list[i] + list[i - 1];
     list.push(a);
   }
-  // console.log(list);
   return list.filter(i => i % 2 == 0).reduce((a, b) => a + b);
 }
 
